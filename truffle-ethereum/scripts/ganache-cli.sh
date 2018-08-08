e687a69ef31d675375bcf2af66930925f45e3993
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2018-08-08 19:41:34 -0300

    Improved manage script

--- truffle-ethereum/scripts/ganache-cli.sh
+++ truffle-ethereum/scripts/ganache-cli.sh
@@ -2,10 +2,6 @@
 
 ganache_port=8545
 
-ganache_running() {
-  nc -z localhost "$ganache_port"
-}
-
 start_ganache() {
   ./node_modules/.bin/ganache-cli -p $ganache_port -m gravity top burden flip student usage spell purchase hundred improve check genre > /dev/null 2>&1 &
   ganache_pid=$!
@@ -13,9 +9,5 @@ start_ganache() {
   echo $ganache_pid > ganache.pid
 }
 
-if ganache_running; then
-  echo "Using existing ganache instance at port $ganache_port"
-else
-  echo "Starting our own ganache instance at port $ganache_port"
-  start_ganache
-fi
+echo "Starting our own ganache instance at port $ganache_port"
+start_ganache
