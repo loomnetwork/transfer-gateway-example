c7f8a174b799f25becfcc34d72fa8ddba4facef3
sarah <sarahg.gray@gmail.com>
2018-09-05 15:55:01 -0500

    set up loom deploy of our new 721 contract

--- truffle-dappchain/migrations/2_deploy_contracts.js
+++ truffle-dappchain/migrations/2_deploy_contracts.js
@@ -2,8 +2,11 @@ const { writeFileSync, readFileSync } = require('fs')
 
 const CryptoCardsDappChain = artifacts.require('CryptoCardsDappChain')
 const GameTokenDappChain = artifacts.require('GameTokenDappChain')
+const FakeLoomCryptoKitty = artifacts.require('FakeLoomCryptoKitty');
 
 module.exports = (deployer, network, accounts) => {
+    // remind me where did this come from? it's not the solidity gateway
+    // and its part of the repo...
   const gatewayAddress = readFileSync('../gateway_dappchain_address', 'utf-8')
 
     // 
@@ -18,4 +21,11 @@ module.exports = (deployer, network, accounts) => {
     console.log(`GameTokenDappChain deployed at address: ${GameTokenDappChainInstance.address}`)
     writeFileSync('../game_token_dappchain_address', GameTokenDappChainInstance.address)
   })
+
+    deployer.deploy(FakeLoomCryptoKitty, gatewayAddress).then(async () => {
+        const FakeLoomCryptoKittyInstance = await FakeLoomCryptoKitty.deployed();
+        console.log(`FakeLoomCryptoKitty deployed at ${FakeLoomCryptoKittyInstance.address}`);
+        writeFileSync('../fake_kitty_loom_address', FakeLoomCryptoKittyInstance.address);
+    })
 }
+
